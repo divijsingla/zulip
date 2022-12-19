c94610808f22ed4f77101a5e702c8a2bfd96095f
Aman Agrawal <amanagr@zulip.com>
2022-12-19 08:16:05 +0000

    floating_recipient_bar: Replace with sticky header.

--- static/js/message_scroll.js
+++ static/js/message_scroll.js
@@ -1,7 +1,6 @@
 import $ from "jquery";
 import _ from "lodash";
 
-import * as floating_recipient_bar from "./floating_recipient_bar";
 import * as hash_util from "./hash_util";
 import * as loading from "./loading";
 import * as message_fetch from "./message_fetch";
@@ -39,7 +38,6 @@ export function show_loading_older() {
     if (!loading_older_messages_indicator_showing) {
         loading.make_indicator($("#loading_older_messages_indicator"), {abs_positioned: true});
         loading_older_messages_indicator_showing = true;
-        floating_recipient_bar.hide();
     }
 }
 
@@ -55,7 +53,6 @@ export function show_loading_newer() {
         $(".bottom-messages-logo").show();
         loading.make_indicator($("#loading_newer_messages_indicator"), {abs_positioned: true});
         loading_newer_messages_indicator_showing = true;
-        floating_recipient_bar.hide();
     }
 }
 
@@ -188,6 +185,7 @@ export function is_actively_scrolling() {
 
 export function scroll_finished() {
     actively_scrolling = false;
+    message_lists.current.view.update_sticky_recipient_headers();
     hide_scroll_to_bottom();
 
     if (recent_topics_util.is_visible()) {
@@ -199,8 +197,6 @@ export function scroll_finished() {
     } else {
         update_selection_on_next_scroll = true;
     }
-
-    floating_recipient_bar.update();
 
     if (message_viewport.at_top()) {
         message_fetch.maybe_load_older_messages({
